a1a226074f3858bcbb9b89feecc90407f1abbaff
Rahul Gambhir <gambhirrahul0@gmail.com>
2020-06-09 00:06:44 +0530

    Done with changes

--- Rahul/1. odd or even.c
+++ Rahul/1. odd or even.c
@@ -5,8 +5,10 @@ void main()
 {
     int a;
     printf("Enter a number: ");
+    //taking a number from user
     scanf("%d",&a);
     if(a%2==0)
+        //checking divisibility rule
     {
         printf("Number is even");
     }
